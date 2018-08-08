5dafeeb44cb377fe4cffeed6bb045d6feb78ceeb
“Tyler <tylerpalef@gmail.com>
2018-08-08 14:36:26 -0400

    finished the contest page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
   def welcome
-    render :welcome
   end
 
   def about
-    render :about
   end
+
+  def contest
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
   get '/' => 'pages#welcome'
   get '/about' => 'pages#about'
+  get '/contest' => 'pages#contest'
 end
